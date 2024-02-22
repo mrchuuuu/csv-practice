a9b146748577ecc8463539b04b1921ed2b086f9e
mrchuuuu <136549998+mrchuuuu@users.noreply.github.com>
2024-02-22 13:16:20 +0800

    Update csvAdd.py

--- csv-practice/files/csvAdd.py
+++ csv-practice/files/csvAdd.py
@@ -1,23 +1,7 @@
 import csv
 import time
 
-def csv_add():
-    
-    studentFile = open('student.csv', 'a')
-    writeData = csv.writer(studentFile)
-
-    studentName = input('Enter Student Name: ')
-    studentId = input('Enter Student ID# YYYY-MMMM: ')
-    studentYear = input('Enter Student Year Level: ')
-    studentGender = input('Enter Student Gender: ')
-
-    studentData = [studentName, studentId, studentYear, studentGender]
-    writeData.writerow(studentData)
-         
-    studentFile.close()    
-        
-if __name__ == "__main__":
-    print("Program Executing..")
+def csv_main():
     
     theLoop = True
     while theLoop:
@@ -31,7 +15,36 @@ if __name__ == "__main__":
 
         else :
             theLoop = False
-        
+    
     time.sleep(2) 
     print("Data Entered!!")
+    
+def csv_add():
+    
+    with open('./data/studentData.csv', 'a') as studentFile:
+        writeData = csv.writer(studentFile, delimiter = '\t')
+        
+        studentName = input('Enter Student Name: ')
+        studentId = input('Enter Student ID# YYYY-MMMM: ')
+        studentYear = input('Enter Student Year Level: ')
+        studentGender = input('Enter Student Gender: ')
+    
+        studentData = [studentName, studentId, studentYear, studentGender]
+        writeData.writerow(studentData)
+        
+        studentFile.close()    
+        
+if __name__ == "__main__":
+
+    print("Program Executing..")
+    time.sleep(2)
+    
+    csv_main()    
+    
+    
+    
+    
+    
+        
+    
     
